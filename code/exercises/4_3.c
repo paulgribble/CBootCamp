81d26ea081c4560977021c39b15dda15855b994c
Paul Gribble <paul@gribblelab.org>
2016-11-27 11:13:09 -0500

    4_3

--- code/exercises/4_3.c
+++ code/exercises/4_3.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
   int n=6;
   int i, j;
-  for (i=0; i<=n; i++) {
+  for (i=1; i<=n; i++) {
     for (j=0; j<(n-i); j++) { printf(" "); }
     for (j=(n-i); j<(n-i)+((2*i)-1); j++) { printf("*"); }
     printf("\n");
